bd879c978dc45d624e36df4faa9950411b4d874f
Pranay Yadav <pranayyadav08@gmail.com>
2022-01-18 19:23:55 +0530

    initial commit

--- backend/server.ts
+++ backend/server.ts
@@ -53,6 +53,8 @@ app.use(notFound);
 
 app.use(errorHandler);
 
+console.log(process.env.PORT);
+
 connectDB().then(() => {
   app.listen(Number(process.env.PORT) || 5000, () => {
     console.log(`Server running successfully`.yellow.bold);
